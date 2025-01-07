bd51b8a697c8ae57af7983492bab0fa77f3525a7
Diurno <Diurno@C06PC29.informatica.comercio>
2025-01-07 13:24:33 +0100

    Primer Commit

---
+++ src/main/java/modelo/ConexionSqlServer.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ *
+ * @author Diurno
+ */
+public class ConexionSqlServer {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://yourserver.database.windows.net:1433;"
+                        + "database=AdventureWorks;"
+                        + "user=yourusername@yourserver;"
+                        + "password=<password>;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            // Code here.
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
